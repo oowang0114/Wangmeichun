3c74372ab9dd06c2d68f99162c8b4f23d1ad885f
WangMeichun <wmcyx123@163.com>
2020-12-13 23:13:11 +0800

    Add existing file

---
+++ antd-demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
